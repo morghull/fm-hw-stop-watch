ddb182eae85d45ebc4c38b349d6200b0af3c9347
morghull <morghull@gmail.com>
2021-11-17 02:16:00 +0200

    move components from main project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component, useState } from 'react';
+import StopWatch from './components/StopWatch';
 
-function App() {
+function App(props) {
+  const [isVisible, setVisible] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={() => setVisible(!isVisible)}>switch</button>
+      {isVisible ? <StopWatch /> : null}
+    </>
   );
 }
 
---
+++ src/components/StopWatch/index.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import styles from './stop-watch.module.css';
+
+class StopWatch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date(0, 0, 0, 0, 0, 0, 0),
+    };
+    this.intervalId = null;
+  }
+  tick = () => {
+    this.setState((state, props) => {
+      const { time } = state;
+      const newTime = new Date(time.getTime() + 1000);
+      return { time: newTime };
+    });
+  };
+  start = () => {
+    if (!this.intervalId)
+      this.intervalId = setInterval(this.tick, 1000);
+  };
+  stop = () => {
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+  };
+  reset = () => {
+    this.stop();
+    this.setState({ time: new Date(0, 0, 0, 0, 0, 0, 0) });
+  };
+  componentDidMount() {
+    this.start();
+  }
+  componentDidUpdate() {}
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+  }
+  render() {
+    const { time } = this.state;
+    return (
+      <article className={styles.container}>
+        <h2>{time.toLocaleTimeString('en-GB')}</h2>
+        <button onClick={this.start}>start</button>
+        <button onClick={this.stop}>stop</button>
+        <button onClick={this.reset}>reset</button>
+      </article>
+    );
+  }
+}
+
+export default StopWatch;
